9e802081b85a44dc946c2f07fdb75bb8208c7221
mastainvin <vincent.mastain@outlook.fr>
2024-02-16 01:50:16 +0100

    first comm tests

--- tests/godot_connection_test.py
+++ tests/godot_connection_test.py
@@ -1,18 +1,6 @@
+import random
 import socket
 import json
-
-# Create a socket object
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Bind socket to address and port
-serverSocket.bind(('127.0.0.1', 12345))
-
-# Listen for incoming connections
-serverSocket.listen(5)
-
-print("Server listening on port 12345")
 
 
 def parse_json_with_eval(json_obj):
@@ -32,22 +20,70 @@ def parse_json_with_eval(json_obj):
         return json_obj
 
 
-try:
+host = '127.0.0.1'
+sport = 8080
 
-    # Accept connection from client
-    clientSocket, address = serverSocket.accept()
-    print(f"Connection from {address} has been established.")
+# Connect to the server -> GAME ENGINE SERVER
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, sport))
+    data = {
+        "command": "start_scenario",
+        "name": "PipeHeightStage",
+    }
+    s.send(json.dumps(data).encode())
 
-    while True:
-        data = clientSocket.recv(1024).decode()
-        data = json.loads(data)
-        data = parse_json_with_eval(data)
-        print(data)
+    data = s.recv(1024)
+    data = data.decode()
+    data = json.loads(data)
+    data = parse_json_with_eval(data)
 
-        clientSocket.send('{"test": 1}'.encode())
-except KeyboardInterrupt:
-    print("Server shutting down")
-    if clientSocket:
-        clientSocket.close()
-    if serverSocket:
-        serverSocket.close()
+    port = data["port"]
+
+    # Connect to server -> SCENE INSTANCE SERVER
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
+        print(port)
+        s2.connect((host, port))
+        try:
+            data = {
+                "args": {
+                    "when": {
+                        "speed": 0,
+                        "position": {
+                            "x": 0,
+                            "y": 0
+                        }
+                    },
+                },
+            }
+
+            s2.send(json.dumps(data).encode())
+
+            while True:
+                data = s2.recv(1024, ).decode()
+                data = json.loads(data)
+                data = parse_json_with_eval(data)
+                print(data)
+
+                # do_jump one time out of 100
+                do_jump = random.randint(0, 50) == 0
+
+                if do_jump:
+                    event = {
+                        "inputs": [
+                            {"type": "JOY_BUTTON_EVENT", "button": "A"},
+                        ],
+                    }
+                else:
+                    event = {
+                        "inputs": [
+                        ],
+                    }
+
+                s2.send(json.dumps(event).encode())
+
+        except:
+            print("Server shutting down")
+            if s:
+                s.close()
+            if s2:
+                s2.close()
